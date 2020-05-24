116e7f8bd80a10ae373020f68b3e770e60fa2f9d
Pdani001 <pecsidaniel10@gmail.com>
2020-05-24 18:45:12 +0200

    Code cleanup + README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# FMCRules plugin
+**Rules plugin written for FNaF MC**
+
+**_Created by Pdani001_**
+
+For help in configuration visit the [config.yml](https://github.com/Pdani001/FMCRules/blob/master/src/config.yml) page.
--- src/hu/Pdani/FMCRules/util/QuizManager.java
+++ src/hu/Pdani/FMCRules/util/QuizManager.java
@@ -41,151 +41,8 @@ public class QuizManager {
         if(plugin.getStatus(player))
             return;
         FRPlayer frp = new FRPlayer(player);
-        RandomCollection<String> rcidlist = new RandomCollection<>();
-        double weight = 100.00 / (double) plugin.getIdlist().size();
-        for(String id : plugin.getIdlist()){
-            rcidlist.add(weight,id);
-        }
-        try {
-            frp.newId(rcidlist.next());
-        } catch (FRException e) {
-            plugin.getLogger().warning(e.toString());
-            try {
-                frp.newId(rcidlist.next());
-            } catch (FRException ex) {
-                plugin.getLogger().severe(e.toString());
-                player.sendMessage(c(plugin.getConfig().getString("messages.error.config")));
-                return;
-            }
-        }
-        List<String> goodlist = new ArrayList<>();
-        List<String> wronglist = new ArrayList<>();
-        int size = (plugin.getConfig().getBoolean("quiz.answers.random",true)) ? 54 : 27;
-        Inventory myInv = plugin.getServer().createInventory(new FRHolder(), size, c(frp.getTitle()));
-        Random rand = new Random();
-        int maxgood = plugin.getConfig().getInt("quiz.answers.good");
-        if(maxgood > plugin.getConfig().getStringList("questions."+frp.getId()+".good").size())
-            maxgood = plugin.getConfig().getStringList("questions."+frp.getId()+".good").size();
-        int maxwrong = plugin.getConfig().getInt("quiz.answers.wrong");
-        if(maxwrong > plugin.getConfig().getStringList("questions."+frp.getId()+".wrong").size())
-            maxwrong = plugin.getConfig().getStringList("questions."+frp.getId()+".wrong").size();
-        String item = frp.newItem();
-        if(plugin.getConfig().getBoolean("quiz.answers.random",true)) {
-            for (int i = 0; i < maxgood; i++) {
-                int r = rand.nextInt(54);
-                if (myInv.getItem(r) != null && myInv.getItem(r).getType() != Material.AIR) {
-                    while (myInv.getItem(r) != null && myInv.getItem(r).getType() != Material.AIR) {
-                        r = rand.nextInt(54);
-                    }
-                }
-                Material ai;
-                try {
-                    ai = Material.valueOf(item);
-                } catch (IllegalArgumentException e) {
-                    ai = Material.STONE;
-                }
-                ItemStack stack = new ItemStack(ai, 1);
-                ItemMeta meta = stack.getItemMeta();
-                String name = frp.newGood();
-                if (goodlist.contains(name)) {
-                    while (goodlist.contains(name)) {
-                        name = frp.newGood();
-                    }
-                }
-                meta.setDisplayName(c(name));
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                stack.setItemMeta(meta);
-                myInv.setItem(r, stack);
-                goodlist.add(name);
-            }
-            for (int i = 0; i < maxwrong; i++) {
-                int r = rand.nextInt(54);
-                if (myInv.getItem(r) != null && myInv.getItem(r).getType() != Material.AIR) {
-                    while (myInv.getItem(r) != null && myInv.getItem(r).getType() != Material.AIR) {
-                        r = rand.nextInt(54);
-                    }
-                }
-                Material ai;
-                try {
-                    ai = Material.valueOf(item);
-                } catch (IllegalArgumentException e) {
-                    ai = Material.STONE;
-                }
-                ItemStack stack = new ItemStack(ai, 1);
-                ItemMeta meta = stack.getItemMeta();
-                String name = frp.newWrong();
-                if (wronglist.contains(name)) {
-                    while (wronglist.contains(name)) {
-                        name = frp.newWrong();
-                    }
-                }
-                meta.setDisplayName(c(name));
-                meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
-                stack.setItemMeta(meta);
-                myInv.setItem(r, stack);
-                wronglist.add(name);
-            }
-        } else {
-            String questItem = frp.newItem();
-            while (questItem.equalsIgnoreCase(item)){
-                questItem = frp.newItem();
-            }
-            Material ai;
-            try {
-                ai = Material.valueOf(item);
-            } catch (IllegalArgumentException e) {
-                ai = Material.STONE;
-            }
-            List<Integer> taken = new ArrayList<>();
-            for(int i = 0; i < 4; i++) {
-                int pos = rand.nextInt(4);
-                while (taken.contains(pos)){
-                    pos = rand.nextInt(4);
-                }
-                if(rand.nextInt(2) == 1 && goodlist.size() < 2 || wronglist.size() == 2) {
-                    String name = frp.newGood();
-                    if (goodlist.contains(name)) {
-                        while (goodlist.contains(name)) {
-                            name = frp.newGood();
-                        }
-                    }
-                    ItemStack stack = new ItemStack(ai, 1);
-                    ItemMeta meta = stack.getItemMeta();
-                    meta.setDisplayName(c(name));
-                    meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                    stack.setItemMeta(meta);
-                    myInv.setItem(intlist[pos], stack);
-                    goodlist.add(name);
-                } else {
-                    String name = frp.newWrong();
-                    if (wronglist.contains(name)) {
-                        while (wronglist.contains(name)) {
-                            name = frp.newWrong();
-                        }
-                    }
-                    ItemStack stack = new ItemStack(ai, 1);
-                    ItemMeta meta = stack.getItemMeta();
-                    meta.setDisplayName(c(name));
-                    meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
-                    stack.setItemMeta(meta);
-                    myInv.setItem(intlist[pos], stack);
-                    wronglist.add(name);
-                }
-                taken.add(pos);
-            }
-            try {
-                ai = Material.valueOf(questItem);
-            } catch (IllegalArgumentException e) {
-                ai = Material.STONE;
-            }
-            ItemStack stack = new ItemStack(ai, 1);
-            ItemMeta meta = stack.getItemMeta();
-            meta.setDisplayName(c(frp.getQuestion()));
-            stack.setItemMeta(meta);
-            myInv.setItem(4, stack);
-        }
-        player.openInventory(myInv);
-        quizlist.put(player,frp);
+        if(getQuiz(frp))
+            quizlist.put(player,frp);
     }
 
     /**
@@ -208,6 +65,17 @@ public class QuizManager {
             return;
         }
         stopClose.add(player);
+        if(getQuiz(frp))
+            quizlist.replace(player,frp);
+    }
+
+    /**
+     * Open a quiz for the Player
+     * @param frp the Player
+     * @since 1.3.0
+     */
+    private boolean getQuiz(FRPlayer frp){
+        Player player = frp.getPlayer();
         RandomCollection<String> rcidlist = new RandomCollection<>();
         double weight = 100.00 / (double) plugin.getIdlist().size();
         for(String id : plugin.getIdlist()){
@@ -234,7 +102,7 @@ public class QuizManager {
             } catch (FRException ex) {
                 plugin.getLogger().severe(e.toString());
                 player.sendMessage(c(plugin.getConfig().getString("messages.error.config")));
-                return;
+                return false;
             }
         }
         List<String> goodlist = new ArrayList<>();
@@ -364,7 +232,7 @@ public class QuizManager {
             myInv.setItem(4, stack);
         }
         player.openInventory(myInv);
-        quizlist.replace(player,frp);
+        return true;
     }
 
     /**
